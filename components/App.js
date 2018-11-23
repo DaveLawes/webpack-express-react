b6bab8e1186fd38fed3f7428de5b779f4110369f
David Lawes <davidlawes@hotmail.co.uk>
2018-11-23 09:52:34 +0000

    setup: added App component

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class App extends Component {
+  render() {
+    return(
+      <h1>This view is being rendered by React and served by Express</h1>
+    )
+  }
+}
+
+export default App;
